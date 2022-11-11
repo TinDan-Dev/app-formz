b3c055607b3ca4ccff73d993904299dc12bea5ab
LeFrosch <daniel-brauner@outlook.com>
2022-11-11 12:21:09 +0100

    [add] notification on cancel

--- formz/lib/src/functional/task/cancellation_token.dart
+++ formz/lib/src/functional/task/cancellation_token.dart
@@ -1,20 +1,39 @@
+import 'package:flutter/foundation.dart';
+
 /// Used by actions that can be cancelled.
-abstract class CancellationReceiver {
+abstract class CancellationReceiver implements ChangeNotifier {
+  const CancellationReceiver();
+
   const factory CancellationReceiver.unused() = _UnusedCancellationReceiver;
 
   /// Whether the action should stop executing.
   bool get canceled;
 }
 
-class _UnusedCancellationReceiver implements CancellationReceiver {
+class _UnusedCancellationReceiver extends CancellationReceiver {
   const _UnusedCancellationReceiver();
 
   @override
   bool get canceled => false;
+
+  @override
+  bool get hasListeners => false;
+
+  @override
+  void addListener(VoidCallback listener) {}
+
+  @override
+  void dispose() {}
+
+  @override
+  void notifyListeners() {}
+
+  @override
+  void removeListener(VoidCallback listener) {}
 }
 
 /// Used to cancel an ongoing action.
-class CancellationToken implements CancellationReceiver {
+class CancellationToken extends CancellationReceiver with ChangeNotifier {
   /// Whether the token has been canceled or not.
   @override
   bool get canceled => _canceled;
@@ -27,5 +46,8 @@ class CancellationToken implements CancellationReceiver {
   void cancel() {
     assert(!_canceled, 'A cancellation token can not be canceled twice');
     _canceled = true;
+
+    notifyListeners();
+    dispose();
   }
 }
